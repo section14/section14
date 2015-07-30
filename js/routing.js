80deba0f74d5dceca2e509150f54827d1d27de86
section14 <henduism@gmail.com>
2015-07-30 16:24:49 -0400

    Bandaided routing problem
    Weird routing problem in any browser but desktop chrome. Hard coded
    project links for now

--- js/routing.js
+++ js/routing.js
@@ -7,10 +7,62 @@ window.addEventListener("WebComponentsReady", function() {
         app.pageContent = "/general/home.html";
     });
 
+    /*
     page('/project/:id', function (ctx) {
         app.route = "project";
         app.pageContent = "/general/project.html";
         app.projectId = ctx.params.id;
+    });
+    */
+
+    page('/project/arduino', function () {
+        app.route = "project";
+        app.pageContent = "/projects/arduino.html";
+    });
+
+    page('/project/audio', function () {
+        app.route = "project";
+        app.pageContent = "/projects/audio.html";
+    });
+
+    page('/project/big_fat_d', function () {
+        app.route = "project";
+        app.pageContent = "/projects/big_fat_d.html";
+    });
+
+    page('/project/logos', function () {
+        app.route = "project";
+        app.pageContent = "/projects/logos.html";
+    });
+
+    page('/project/openframeworks', function () {
+        app.route = "project";
+        app.pageContent = "/projects/openframeworks.html";
+    });
+
+    page('/project/spectre', function () {
+        app.route = "project";
+        app.pageContent = "/projects/spectre.html";
+    });
+
+    page('/project/spectre_dev', function () {
+        app.route = "project";
+        app.pageContent = "/projects/spectre_dev.html";
+    });
+
+    page('/project/ssr', function () {
+        app.route = "project";
+        app.pageContent = "/projects/ssr.html";
+    });
+
+    page('/project/tshirts', function () {
+        app.route = "project";
+        app.pageContent = "/projects/tshirts.html";
+    });
+
+    page('/project/vector', function () {
+        app.route = "project";
+        app.pageContent = "/projects/vector.html";
     });
 
     page('/about', function () {
